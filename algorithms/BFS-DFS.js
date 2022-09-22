c4c498a67bba5a6f024e263526a7f9a9f1f86849
Michelle Lin <m971119@gmail.com>
2022-09-22 21:44:48 +0800

    feat: implement search algos on BST - DFS and BFS

---
+++ algorithms/BFS-DFS.js
@@ -0,0 +1,253 @@
+class Node {
+    constructor(value) {
+        this.left = null;
+        this.right = null;
+        this.value = value;
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+    insert(value) {
+        const newNode = new Node(value);
+        if (this.root === null) {
+            this.root = newNode;
+            return this;
+        }
+        /*
+        // My solution: 空間利用較差
+        const DIRECTION_RIGHT = 'right';
+        const DIRECTION_LEFT = 'left';
+        let currentNode = this.root;
+        let lastNode = null;
+        let direction = null;
+        while (currentNode !== null) {
+            lastNode = currentNode;
+            if (newNode.value > currentNode.value) {
+                currentNode = currentNode.right;
+                direction = DIRECTION_RIGHT;
+            } else {
+                currentNode = currentNode.left;
+                direction = DIRECTION_LEFT;
+            }
+        }
+        lastNode[direction] = newNode;
+        */
+        // Teacher's solution
+        let currentNode = this.root;
+        while (true) {
+            if (value < currentNode.value) {
+                if (!currentNode.left) {
+                    currentNode.left = newNode;
+                    return this;
+                }
+                currentNode = currentNode.left;
+            } else {
+                if (!currentNode.right) {
+                    currentNode.right = newNode;
+                    return this;
+                }
+                currentNode = currentNode.right;
+            }
+        }
+        return this;
+    }
+    lookup(value) {
+        let currentNode = this.root;
+        while (currentNode !== null) {
+            if (currentNode.value === value) {
+                return currentNode;
+            }
+            if (value > currentNode.value) {
+                currentNode = currentNode.right;
+            } else {
+                currentNode = currentNode.left;
+            }
+        }
+        return false;
+    }
+    remove(value) {
+        if (!this.root) {
+            return false;
+        }
+        let currentNode = this.root;
+        let parentNode = null;
+        while (currentNode) {
+            if (value < currentNode.value) {
+                parentNode = currentNode;
+                currentNode = currentNode.left;
+            } else if (value > currentNode.value) {
+                parentNode = currentNode;
+                currentNode = currentNode.right;
+            } else if (currentNode.value === value) {
+                //We have a match, get to work!
+
+                //Option 1: No right child: 
+                if (currentNode.right === null) {
+                    if (parentNode === null) {
+                        this.root = currentNode.left;
+                    } else {
+
+                        //if parent > current value, make current left child a child of parent
+                        if (currentNode.value < parentNode.value) {
+                            parentNode.left = currentNode.left;
+
+                            //if parent < current value, make left child a right child of parent
+                        } else if (currentNode.value > parentNode.value) {
+                            parentNode.right = currentNode.left;
+                        }
+                    }
+
+                    //Option 2: Right child which doesnt have a left child
+                } else if (currentNode.right.left === null) {
+                    currentNode.right.left = currentNode.left;
+                    if (parentNode === null) {
+                        this.root = currentNode.right;
+                    } else {
+
+                        //if parent > current, make right child of the left the parent
+                        if (currentNode.value < parentNode.value) {
+                            parentNode.left = currentNode.right;
+
+                            //if parent < current, make right child a right child of the parent
+                        } else if (currentNode.value > parentNode.value) {
+                            parentNode.right = currentNode.right;
+                        }
+                    }
+
+                    //Option 3: Right child that has a left child
+                } else {
+
+                    //find the Right child's left most child
+                    let leftmost = currentNode.right.left;
+                    let leftmostParent = currentNode.right;
+                    while (leftmost.left !== null) {
+                        leftmostParent = leftmost;
+                        leftmost = leftmost.left;
+                    }
+
+                    //Parent's left subtree is now leftmost's right subtree
+                    leftmostParent.left = leftmost.right;
+                    leftmost.left = currentNode.left;
+                    leftmost.right = currentNode.right;
+
+                    if (parentNode === null) {
+                        this.root = leftmost;
+                    } else {
+                        if (currentNode.value < parentNode.value) {
+                            parentNode.left = leftmost;
+                        } else if (currentNode.value > parentNode.value) {
+                            parentNode.right = leftmost;
+                        }
+                    }
+                }
+                return true;
+            }
+        }
+    }
+    breadthFirstSearch() {
+        /**
+         *  Takes up more memory (bc of the queue)
+         */
+        let currentNode = this.root;
+        let list = [];
+        let queue = [];
+        queue.push(currentNode);
+        while(queue.length > 0) {
+            // shift() -> removes the first item in the queue
+            currentNode = queue.shift();
+            list.push(currentNode.value);
+            if (currentNode.left) {
+                queue.push(currentNode.left);
+            }
+            if (currentNode.right) {
+                queue.push(currentNode.right);
+            }
+        }
+        return list;
+    }
+    breadthFirstSearchR(queue, list) {
+        if (!queue.length) {
+            return list;
+        }
+        const currentNode = queue.shift();
+        list.push(currentNode.value);
+        if (currentNode.left) {
+            queue.push(currentNode.left);
+        }
+        if (currentNode.right) {
+            queue.push(currentNode.right);
+        }
+        return this.breadthFirstSearchR(queue, list);
+    }
+    DFSInOrder() {
+        return traverseInOrder(this.root, []);
+    }
+    DFSPostOrder() {
+        return traversePostOrder(this.root, []);
+    }
+    DFSPreOrder() {
+        return traversePreOrder(this.root, []);
+    }
+}
+
+function traverseInOrder(node, list) {
+    if (node.left) {
+        traverseInOrder(node.left, list);
+    }
+    list.push(node.value);
+    if (node.right) {
+        traverseInOrder(node.right, list);
+    }
+    return list;
+}
+
+function traversePreOrder(node, list) {
+    list.push(node.value);
+    if (node.left) {
+        traversePreOrder(node.left, list);
+    }
+    if (node.right) {
+        traversePreOrder(node.right, list);
+    }
+    return list;
+}
+
+function traversePostOrder(node, list) {
+    if (node.left) {
+        traversePostOrder(node.left, list);
+    }
+    if (node.right) {
+        traversePostOrder(node.right, list);
+    }
+    list.push(node.value);
+    return list;
+}
+//      9
+//  4      20
+// 1  6  15  170
+const tree = new BinarySearchTree();
+tree.insert(9);
+tree.insert(4);
+tree.insert(6);
+tree.insert(20);
+tree.insert(170);
+tree.insert(15);
+tree.insert(1);
+
+
+console.log(tree.breadthFirstSearch());
+console.log(tree.breadthFirstSearchR([tree.root], []));
+console.log(tree.DFSInOrder());
+console.log(tree.DFSPreOrder());
+console.log(tree.DFSPostOrder());
+//      9
+//  4      20
+// 1  6  15  170
+
+// Three types of DFS
+// InOrder = [1, 4, 6, 9, 15, 20, 170]
+// PreOrder = [9, 4, 1, 6, 20, 15, 170] --> used to recreate the tree really easily
+// PostOrder = [1, 6, 4, 15, 170, 20, 9]
